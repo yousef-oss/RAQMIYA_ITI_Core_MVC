f90fa1871ee82c8da0d2a996d796ea2807342496
devMahmoudEmad <mahmoudemad7521@gmail.com>
2025-07-07 04:30:01 +0300

    space fix

--- Models/ProductCategory.cs
+++ Models/ProductCategory.cs
@@ -6,7 +6,7 @@
         public int CategoryId { get; set; }
 
         public Product Product { get; set; } = null!;
-        public Category Category { get; set; } = null!;
+        public Category Category { get; set; } = null!; 
     }
 
 }
