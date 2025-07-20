448dc20f3b4cbdec67ba3ba9100e97b3efee62ab
devMahmoudEmad <mahmoudemad7521@gmail.com>
2025-07-20 15:16:54 +0300

    login and register controllers linked to the _layout view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,16 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Terms()
+    {
+        return View();
+    }
+
+    public IActionResult Cookie()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
